d10aa0d6144ad8b7554c3df5d8af6e7960d7536e
Felix Jurado <felixlaurajan@gmail.com>
2022-06-04 16:51:18 +0200

    ultimos cambios

---
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -0,0 +1,37 @@
+
+
+import React from 'react'
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
+import { LazyPage1,LazyPage2,LazyPage3 } from '../pages';
+
+export const LazyLayout = () => {
+  return (
+    <div>
+        <h1>LazyLayout pages</h1>
+
+            <ul>
+                <li>
+                    <NavLink to="lazy1">Lazy1</NavLink>
+                </li>
+                <li>
+                    <NavLink to="lazy2">Lazy2</NavLink>
+                </li>
+                <li>
+                    <NavLink to="lazy3">Lazy3</NavLink>
+                </li>
+            </ul>
+        
+        <Routes>
+            <Route path='lazy1' element={<LazyPage1/>}></Route>
+            <Route path='lazy2' element={<LazyPage2/>}></Route>
+            <Route path='lazy3' element={<LazyPage3/>}></Route>
+
+            <Route path='*' element={<Navigate replace to="lazy1"/>}></Route>
+        </Routes>
+
+    </div>
+  )
+}
+
+export default LazyLayout;
+
